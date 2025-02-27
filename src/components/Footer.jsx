cb5cc2fa5d45d1df024bff5940cfe9a60f2c0b97
JonhSmith7777 <hengsamnang7077@gmail.com>
2025-02-27 21:42:34 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className="w-full bg-black h-32 text-white flex justify-center items-center">
-        <div>Edit Footer</div>
+        <div>Edit Footer 6</div>
       </div>
   )
 }
